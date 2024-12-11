287b1c01d81f89f1a6370814c86cae4927563dbc
adriannole <101682107+adriannole@users.noreply.github.com>
2024-12-11 08:56:38 -0500

    up

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 def create_app():
     app = Flask(__name__)
 
+    @app.route('/', methods=['GET'])
+    def home():
+        # Sirve el archivo HTML
+        return render_template('index.html')
+
     @app.route('/api/v1/hello', methods=['GET'])
     def hello_world():
-        return {"message": "Hola Mundo!"}, 200
+        # Devuelve un mensaje JSON
+        return jsonify({"message": "Hola Mundo!"}), 200
 
     return app
